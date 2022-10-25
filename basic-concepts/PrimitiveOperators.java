8084637daf9c209e5b490fe97fff2b8142454b1e
emilly cavalcante <emilly.menezescs@gmail.com>
2022-10-25 17:45:23 -0300

    Update and rename once-in-the-bar.java to PrimitiveOperators.java

--- basic-concepts/PrimitiveOperators.java
+++ basic-concepts/PrimitiveOperators.java
@@ -1,6 +1,6 @@
 // A bartender sold 64 bottles of beer and 23 bottles of whisky. You need to calculate how many total bottles are sold.
 
-public class OnceInTheBar {
+public class PrimitiveOperators {
   public static void main(String[] args) {
 	  
     int beerSold  = 64;
