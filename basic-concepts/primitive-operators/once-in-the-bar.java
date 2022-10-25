ce7081c24e12d3e172a23e0257535bd1910325b9
emilly cavalcante <emilly.menezescs@gmail.com>
2022-10-25 17:19:11 -0300

    Create once-in-the-bar.java

---
+++ basic-concepts/primitive-operators/once-in-the-bar.java
@@ -0,0 +1,14 @@
+// A bartender sold 64 bottles of beer and 23 bottles of whisky. You need to calculate and output how many total bottles are sold.
+
+public class OnceInTheBar {
+  
+  public static void main(String[] args) {
+    
+    int beerSold = 64;
+    int whiskySold = 23;
+    int totalSold = beerSold + whiskySold;
+    
+    System.out.println("Total bottles sold: " + totalSold);
+    
+  }
+}
